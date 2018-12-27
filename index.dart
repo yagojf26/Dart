4c10ce7a5c010c1c1524b66a0baf1824868844be
Yago Santos <yagojf26@gmail.com>
2018-12-27 16:39:57 -0200

    ...

---
+++ index.dart
@@ -0,0 +1,11 @@
+void main() {
+  //para imprimir na tela use: print()
+  // para declarar variavel use variaveis tipadas: 
+  String nome = 'Yago';
+  int ano = 2019;
+  print("hello world " + nome);
+ 
+
+
+
+} 
