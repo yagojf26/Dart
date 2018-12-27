47f828c96ce37cd4e1c74eeabdf9da36a075f2c0
Yago Santos <yagojf26@gmail.com>
2018-12-27 16:41:23 -0200

    criando arquivo dart

--- index.dart
+++ index.dart
@@ -2,7 +2,7 @@ void main() {
   //para imprimir na tela use: print()
   // para declarar variavel use variaveis tipadas: 
   String nome = 'Yago';
-  int ano = 2019;
+
   print("hello world " + nome);
  
 
